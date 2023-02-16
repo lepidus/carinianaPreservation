6e2b6087c6a7575d6d357c7a6b96e547b6d1e974
Jhon <jhon@lepidus.com.br>
2023-02-16 16:32:34 -0400

    Adds integration test to settings form
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#508
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

---
+++ cypress/tests/functional/Test1_pluginSettings.spec.js
@@ -0,0 +1,23 @@
+function loginAdminUser() {
+    cy.get('input[id=username]').clear();
+    cy.get('input[id=username]').type(Cypress.env('OJSAdminUsername'), { delay: 0 });
+    cy.get('input[id=password]').type(Cypress.env('OJSAdminPassword'), { delay: 0 });
+    cy.get('button[class=submit]').click();
+}
+
+describe("Cariniana Preservation Plugin - Checking of plugin's settings", function() {
+    it("Setting of recipient e-mail in plugin's settings", function() {
+        cy.visit(Cypress.env('baseUrl') + 'index.php/' + Cypress.env('journalAcronym') + '/management/settings/website');
+        loginAdminUser();
+
+        cy.get('#plugins-button').click();
+        cy.get('#component-grid-settings-plugins-settingsplugingrid-category-generic-row-carinianapreservationplugin > .first_column > .show_extras').click();
+        cy.get('a[id^=component-grid-settings-plugins-settingsplugingrid-category-generic-row-carinianapreservationplugin-settings-button]').click();
+        
+        cy.get('input[id^=recipientEmail]').clear().type('destino.cariniana@gmail.com');
+        cy.get('button[id^=submitFormButton]').contains('Save').click();
+        
+        cy.get('a[id^=component-grid-settings-plugins-settingsplugingrid-category-generic-row-carinianapreservationplugin-settings-button]').click();
+        cy.get('input[id^=recipientEmail]').should('have.value', 'destino.cariniana@gmail.com');
+    });
+});

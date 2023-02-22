db5bee2a553724f29d6f34b3158b9fa01365c870
Jhon <jhon@lepidus.com.br>
2023-02-22 12:12:53 -0400

    Adds unit tests for spreadsheet class
    Also adds composer file for dependencies
    
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#511
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

---
+++ tests/PreservedJournalSpreadsheetTest.php
@@ -0,0 +1,59 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+
+import('classes.submission.Submission');
+import('plugins.generic.carinianaPreservation.classes.PreservedJournalSpreadsheet');
+
+class PreservedJournalSpreadsheetTest extends TestCase
+{
+    private $spreadsheet;
+    private $filePath = '/tmp/test_spreadsheet.xlsx';
+
+    public function setUp(): void
+    {
+        $journals = [
+            new PreservedJournal('SciELO', 'SciELO Journal n18', '1234-1234', '0101-1010', 'https://scielo-journal-18.com.br/', 'scielojournal18', '2018-2022', 'We are the 18th SciELO journal')
+        ];
+
+        $this->spreadsheet = new PreservedJournalSpreadsheet($journals);
+    }
+
+    public function tearDown(): void
+    {
+        if (file_exists(($this->filePath))) {
+            unlink($this->filePath);
+        }
+    }
+
+    private function getWorksheet()
+    {
+        $reader = IOFactory::createReader('Xlsx');
+        $reader->setReadDataOnly(true);
+        $spreadsheet = $reader->load($this->filePath);
+        return $spreadsheet->getActiveSheet();
+    }
+
+    public function testGeneratedSpreadsheetHasHeaders(): void
+    {
+        $this->spreadsheet->createSpreadsheet($this->filePath);
+        $worksheet = $this->getWorksheet();
+        $expectedHeaders = [
+            __("plugins.generic.carinianaPreservation.headers.publisherOrInstitution"),
+            __("plugins.generic.carinianaPreservation.headers.title"),
+            __("plugins.generic.carinianaPreservation.headers.issn"),
+            __("plugins.generic.carinianaPreservation.headers.eIssn"),
+            __("plugins.generic.carinianaPreservation.headers.baseUrl"),
+            __("plugins.generic.carinianaPreservation.headers.journalPath"),
+            __("plugins.generic.carinianaPreservation.headers.availableYears"),
+            __("plugins.generic.carinianaPreservation.headers.notesAndComments"),
+        ];
+
+        $firstRow = $worksheet->rangeToArray('A1:A8');
+
+        $this->assertEquals($expectedHeaders, $firstRow);
+    }
+}

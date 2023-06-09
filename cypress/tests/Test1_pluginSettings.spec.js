8397c494fd86690bbfdd174a7de0d03677304243
Jhon <jhon@lepidus.com.br>
2023-06-09 15:35:47 -0400

    Updates plugin settings integration test
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#569
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>
    Signed-off-by: Lucas Anselmo <lucasanselmo@lepidus.com.br>

--- cypress/tests/Test1_pluginSettings.spec.js
+++ cypress/tests/Test1_pluginSettings.spec.js
@@ -1,6 +1,7 @@
 describe("Cariniana Preservation Plugin - Checking of plugin's settings", function() {
     it("Sets recipient e-mail and responsability statement", function() {
         const pluginRowId = 'component-grid-settings-plugins-settingsplugingrid-category-generic-row-carinianapreservationplugin';
+        const extraCopyEmail = 'copia.extra.cariniana@gmail.com';
         cy.login('dbarnes', null, 'publicknowledge');
         cy.get('a:contains("Website")').click();
 
@@ -9,8 +10,6 @@ describe("Cariniana Preservation Plugin - Checking of plugin's settings", functi
         cy.get('#' + pluginRowId + ' > .first_column > .show_extras').click();
         cy.get('a[id^='+ pluginRowId + '-settings-button]').click();
         
-        cy.contains('Recipient e-mail');
-        cy.get('input[id^=recipientEmail]').clear().type('destino.cariniana@gmail.com');
         cy.contains('Responsability statement');
         cy.fixture('dummy.pdf', { encoding: 'base64' }).then((fileContent) => {
 			cy.get('#statementUpload input[type=file]')
@@ -22,10 +21,12 @@ describe("Cariniana Preservation Plugin - Checking of plugin's settings", functi
 				});
 		});
         cy.wait(200);
+        cy.contains('Extra copy e-mail');
+        cy.get('input[id^=extraCopyEmail]').clear().type(extraCopyEmail);
         cy.get('button[id^=submitFormButton]').contains('Save').click();
         
         cy.get('a[id^=' + pluginRowId + '-settings-button]').click();
-        cy.get('input[id^=recipientEmail]').should('have.value', 'destino.cariniana@gmail.com');
+        cy.get('input[id^=extraCopyEmail]').should('have.value', extraCopyEmail);
         cy.contains('The completed file of the terms of responsibility has already been sent previously and is saved in the system');
     });
 });
